2e341c59772a1865783aa297c86c6e01e7fddf96
Gelli Arya vardhan <aryahackasol55@gmail.com>
2025-12-13 07:51:00 +0000

    the page is still just plain text

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,22 @@
 import type {NextConfig} from 'next';
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = ''
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
-  trailingSlash: true,
+  assetPrefix: assetPrefix,
+  basePath: basePath,
   typescript: {
     ignoreBuildErrors: true,
   },
